3dad4242bc1d3358e69b551e76e9d7a1b1c09ec4
Soe Zayar <soezayar019@gmail.com>
2022-08-02 19:17:55 +0630

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`nav-tabpanel-${index}`}
+            aria-labelledby={`nav-tab-${index}`}
+            {...other}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            {value === index && (
+                <Box>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
-export default App;
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `nav-tab-${index}`,
+        "aria-controls": `nav-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        padding: "10px 20px",
+    },
+}));
+
+export default function App() {
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" style={{ backgroundColor: "#57BFC4" }}>
+                <Tabs variant="fullWidth" value={value} onChange={handleChange} aria-label="nav tabs">
+                    <Tab label="Create Invoice" {...a11yProps(0)} />
+                    <Tab label="View All Invoices" {...a11yProps(1)} />
+                    <Tab label="Invoices Graph" {...a11yProps(2)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                Tab One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Tab Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Tab Three
+            </TabPanel>
+        </div>
+    );
+}
